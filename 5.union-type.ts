dd734e32459cc432710ee83f4f86ba428b7a50dd
shahsuresh <shahsuresh75@gmail.com>
2025-05-02 22:10:21 +0545

    Intersection type added

--- 5.union-type.ts
+++ 5.union-type.ts
@@ -1,4 +1,4 @@
-//A union type in TypeScript allows a variable to hold more than one type.
+//# A union type in TypeScript allows a variable to hold more than one type.
 let id: string | number; // id can be either a string or a number
 id = "user1";
 id = 101;
@@ -11,3 +11,30 @@ function userId(userid: string | number) {
 userId("hellosuresh1"); // show output
 userId(501); // show output
 // userId(true)  // show error: Argument of type boolean is not assignable
+
+//# Intersection Type
+// an intersection type is used to combine multiple types into one.
+// An object of an intersection type must satisfy all the types involved.
+
+type A = { name: string };
+type B = { age: number };
+
+type C = A & B;
+
+//example
+type Students = {
+  name: string;
+};
+
+type Teacher = Students & {
+  teachingSubject: string;
+  salary: number;
+};
+
+function teacherDetails(teacher: Teacher) {
+  console.log(
+    `Name of teacher is:${teacher.name},salary:${teacher.salary}. He teaches ${teacher.teachingSubject}`
+  );
+}
+
+teacherDetails({ name: "Suresh", teachingSubject: "Math", salary: 40000 });
